a01a0fe35aad5a6ad5e8846aa2109aa06ad1b16a
recursivecodes <toddraymondsharp@gmail.com>
2022-03-29 15:00:09 -0400

    initial commit

--- src/main/java/atp/auto/wallet/fn/Function.java
+++ src/main/java/atp/auto/wallet/fn/Function.java
@@ -1,5 +1,8 @@
 package atp.auto.wallet.fn;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.micronaut.context.annotation.BootstrapContextCompatible;
 import io.micronaut.core.annotation.ReflectiveAccess;
 import io.micronaut.oraclecloud.function.OciFunction;
 import io.micronaut.oraclecloud.core.TenancyIdProvider;
@@ -7,18 +10,36 @@ import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 
 import javax.sql.DataSource;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 @Singleton
 public class Function extends OciFunction {
     @Inject
-    TenancyIdProvider tenantIdProvider;
-
-    @Inject
     DataSource dataSource;
 
     @ReflectiveAccess
-    public String handleRequest() {
-        String tenancyId = tenantIdProvider.getTenancyId();
-        return "Your tenancy is: " + tenancyId;
+    public String handleRequest() throws SQLException, JsonProcessingException {
+        Connection conn = dataSource.getConnection();
+        Statement statement = conn.createStatement();
+        ResultSet resultSet = statement.executeQuery("select id, first_name, last_name from users");
+        return new ObjectMapper().writeValueAsString(convertResultSetToList(resultSet));
     }
+
+    private List<HashMap<String,Object>> convertResultSetToList(ResultSet rs) throws SQLException {
+        ResultSetMetaData md = rs.getMetaData();
+        int columns = md.getColumnCount();
+        List<HashMap<String,Object>> list = new ArrayList<HashMap<String,Object>>();
+        while (rs.next()) {
+            HashMap<String,Object> row = new HashMap<String, Object>(columns);
+            for(int i=1; i<=columns; ++i) {
+                row.put(md.getColumnName(i),rs.getObject(i));
+            }
+            list.add(row);
+        }
+        return list;
+    }
+
 }
